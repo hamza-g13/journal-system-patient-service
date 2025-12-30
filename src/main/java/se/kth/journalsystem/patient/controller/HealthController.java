dde6fecc733d496a6cbca6020e6f5b68d9b8abaa
hamza-g13 <hamza.nahas123@gmail.com>
2025-12-30 18:34:39 +0100

    git

--- src/main/java/se/kth/journalsystem/patient/controller/HealthController.java
+++ src/main/java/se/kth/journalsystem/patient/controller/HealthController.java
@@ -1,4 +1,4 @@
-package se.kth.journalsystem.auth.controller;
+package se.kth.journalsystem.patient.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
